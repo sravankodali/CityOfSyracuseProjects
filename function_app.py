a84fa9f98d948aeb4d11bbc46a7a41fdb1c37bfa
Sravan Kodali <sravan.kodali@cbasyr.org>
2024-09-04 21:37:42 -0400

    got rid of all os actions and got rid of all excess requirements

--- function_app.py
+++ function_app.py
@@ -6,14 +6,6 @@ from azure.storage.blob import BlobServiceClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-# Set up OpenAI API key
-openai.api_key = os.getenv("OpenAIKey")
-
-# Set up Azure Blob Storage connection string
-connection_string = os.getenv("AzureWebJobsStorage")
-container_name = "spreadsheet"
-blob_name = "Bond Master File.xlsx.json"
-
 @app.route(route="TriggerProcessingBonds")
 def TriggerProcessingBonds(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
